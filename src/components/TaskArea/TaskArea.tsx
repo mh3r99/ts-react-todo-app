56c3bbb584b23dc7f438f18e1461db558d977b01
Mher Hovhannisyan <mh3r99@gmail.com>
2023-03-05 14:41:42 +0400

    TaskArea and Sidebar

---
+++ src/components/TaskArea/TaskArea.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+
+const TaskArea = () => {
+  return (
+    <Grid item md={8} px={4}>
+      <h2>Content Area</h2>
+    </Grid>
+  );
+};
+
+export default TaskArea;
